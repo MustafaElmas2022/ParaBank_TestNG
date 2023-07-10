bf4307fdf730ed1d9b63a97cad517192b6cbed27
MustafaElmas2022 <mustafaelmas2019@gmail.com>
2023-07-10 17:07:09 +0200

    Modified BasePage

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,4 +1,10 @@
 package Pages;
 
-public class BasePage {
+import Utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
